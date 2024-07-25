768258fa228126f0e30bd0789cff9ef838897289
Phaominho <123705922+minho0516@users.noreply.github.com>
2024-07-25 18:55:44 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+
+    cout.precision(3);
+    cout << fixed;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
