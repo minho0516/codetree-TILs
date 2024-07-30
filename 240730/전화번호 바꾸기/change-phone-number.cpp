408ae47c2450f4049860a4ee3a87cda0b81e7727
Phaominho <123705922+minho0516@users.noreply.github.com>
2024-07-30 23:21:11 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int zero, first, second;
+    char ch1, ch2;
+
+    cin >> zero >> ch1 >> first >> ch2 >> second;
+    cout << "0" << zero << ch1 << second << ch2 << first;
+    return 0;
+}
