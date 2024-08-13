f66bb74feb9cde10cc5c6e9f9dce6ab3634ca348
Phaominho <123705922+minho0516@users.noreply.github.com>
2024-08-13 23:37:19 +0900

    Title: 음수 구별하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a;
+    cout << endl;
+    
+    if(a >= 0)
+    {
+
+    }
+    else
+    {
+        cout << "minus";
+    }
+    return 0;
+}
