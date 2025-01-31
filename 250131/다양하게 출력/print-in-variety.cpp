8a1e0ed50d58b8662bbac17aa77cbbe0e01cd38d
Phaominho <123705922+minho0516@users.noreply.github.com>
2025-01-31 21:24:48 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250131/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Total days in Year\n365\nCircumference rate\n";
+    cout << std::fixed << std::setprecision(10) << (double)3.1415926535 << endl;
+}
